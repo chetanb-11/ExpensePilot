f541795506ec989a489998f5fa700f48e1f6f15a
jaingautam14 <169168898+jaingautam14@users.noreply.github.com>
2025-09-12 14:36:08 +0530

    Update ExpenseRepo.java

--- backend/src/main/java/com/project/expensepilot/repo/ExpenseRepo.java
+++ backend/src/main/java/com/project/expensepilot/repo/ExpenseRepo.java
@@ -1,8 +1,6 @@
 package com.project.expensepilot.repo;
 
-import com.project.expensepilot.model.Expense;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+
 
 @Repository
 public interface ExpenseRepo extends JpaRepository<Expense, Integer> {
