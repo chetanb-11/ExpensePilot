3b6e60071d90a854bdb29e0012cea173bd61ffb5
jaingautam14 <169168898+jaingautam14@users.noreply.github.com>
2025-09-12 14:34:40 +0530

    Update Expense.java

--- backend/src/main/java/com/project/expensepilot/model/Expense.java
+++ backend/src/main/java/com/project/expensepilot/model/Expense.java
@@ -14,6 +14,10 @@ public class Expense {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+    private double amount;
+    private String type;
+    private String category;
+    private String description;
     
     private String date; // Ideally, use LocalDate for date representation
 
