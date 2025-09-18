1199857ca823f924460e5e6451bab9c21f2b86ff
Chetan <chetan87706@gmail.com>
2025-09-18 16:32:42 +0530

    error solving

--- backend/src/main/java/com/project/expensepilot/service/ExpenseService.java
+++ backend/src/main/java/com/project/expensepilot/service/ExpenseService.java
@@ -13,12 +13,6 @@ public class ExpenseService {
     @Autowired
     private ExpenseRepo expenseRepo;
 
-//    private final List<Expense> expenses = new ArrayList<>(Arrays.asList(
-//            new Expense(1, 75000.00, "Income", "Salary", "Monthly Salary", "2025-09-01"),
-//            new Expense(2, 1500.00, "Expense", "Food", "Lunch", "2025-09-02"),
-//            new Expense(3, 2000.00, "Expense", "Transport", "Bus fare", "2025-09-03")
-//    ));
-
     public void addExpense(Expense expense) {
         expenseRepo.save(expense);
     }
